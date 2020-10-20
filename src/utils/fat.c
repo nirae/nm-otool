8d5b502e7c368d7516d075e438a488029ae4d2c1
nirae <hello@nicolasdubouilh.fr>
2020-10-20 19:23:50 +0200

    fat ok | need architecture print

--- src/utils/fat.c
+++ src/utils/fat.c
@@ -6,7 +6,7 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 17:37:34 by ndubouil          #+#    #+#             */
-/*   Updated: 2020/10/19 15:58:21 by ndubouil         ###   ########.fr       */
+/*   Updated: 2020/10/20 19:22:22 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,21 @@ static int manage_fat(void *file, int t, void *arch)
     int         type;
 
     struct fat_arch *a = ((struct fat_arch *)(arch));
-    if (t & L_ENDIAN)
-    {
-        if (uint32_swap(a->cputype) == CPU_TYPE_X86_64)
-            printf("x86\n");
-        if ((type = is_macho(file + uint32_swap(a->offset))) == FALSE)
-            return (FALSE);
-        object_handler(file + uint32_swap(a->offset), type, uint32_swap(a->size));
-    }
-    else
-    {
-        if (a->cputype == CPU_TYPE_X86_64)
-            printf("x86\n");
-        if ((type = is_macho(file + a->offset)) == FALSE)
-            return (FALSE);
-        object_handler(file + a->offset, type, a->size);
-    }
+    if ((type = is_macho(file + addr_32(a->offset, t))) == FALSE)
+        return (FALSE);
+    object_handler(file + addr_32(a->offset, t), type, addr_32(a->size, t));
 	return (TRUE);
+}
+
+static int is_our_cpu(int type, void *arch)
+{
+    struct fat_arch *a = ((struct fat_arch *)(arch));
+
+    if (addr_32(a->cputype, type) == CPU_TYPE_X86_64)
+    {
+        return (TRUE);
+    }
+	return (FALSE);
 }
 
 int fat_handler(void *file, int type)
@@ -41,9 +39,10 @@ int fat_handler(void *file, int type)
 	struct fat_arch		*arch;
 	uint32_t	        numbers_arch;
     uint32_t                 i;
+    int                 x86;
 	// arch = ((struct fat_header *)(file))->nfat_arch;
 	arch = file + sizeof(struct fat_header);
-    
+    x86 = FALSE;
     if (type & L_ENDIAN)
     {
         numbers_arch = uint32_swap(((struct fat_header *)(file))->nfat_arch);
@@ -55,9 +54,30 @@ int fat_handler(void *file, int type)
     i = 0;
     while (i < numbers_arch)
 	{
+        if (is_our_cpu(type, arch))
+        {
+            x86 = TRUE;
+        }
+	    arch = (void *)arch + sizeof(struct fat_arch);   
+        i++;
+    }
+	arch = file + sizeof(struct fat_header);
+    i = 0;
+    while (i < numbers_arch)
+	{
+        if (addr_32(((struct fat_arch *)(arch))->cputype, type) != CPU_TYPE_X86_64 && x86)
+        {
+	        arch = (void *)arch + sizeof(struct fat_arch);   
+            i++;
+            continue;
+        }
         if (manage_fat(file, type, arch) == FALSE)
         {
             return (FALSE);
+        }
+        if (x86)
+        {
+            break;
         }
 	    arch = (void *)arch + sizeof(struct fat_arch);   
         i++;
